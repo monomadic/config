b2fbcee16cc1bd557e3c2b57da6233c5249c0e8c
nom <nom@nowhere>
2022-01-11 15:50:12 +0800

    nvim stuff

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,22 +6,22 @@ vim.api.nvim_set_keymap('', '<C-q>', ':quit!<CR>', {noremap = true})
 vim.api.nvim_set_keymap('', '<C-p>', ':FZF<CR>', {})
 vim.api.nvim_set_keymap('', '<C-b>', ':NERDTreeToggle<CR>', {})
 vim.api.nvim_set_keymap('n', '<C-j>', '<Plug>(easymotion-bd-w)', {})
+
 -- plugins
 --
 local Plug = vim.fn['plug#']
-vim.call('plug#begin', '~/.config/nvim/plugged')
---Plug('roxma/nvim-completion-manager')
-Plug('junegunn/fzf')
-Plug('scrooloose/nerdtree', {on = 'NERDTreeToggle'})
-Plug('easymotion/vim-easymotion')
+ vim.call('plug#begin', '~/.config/nvim/plugged')
+ --Plug('roxma/nvim-completion-manager')
+ Plug('junegunn/fzf')
+ Plug('scrooloose/nerdtree', {on = 'NERDTreeToggle'})
+ Plug('easymotion/vim-easymotion')
+ Plug('neovim/nvim-lspconfig')
+ Plug('ternjs/tern_for_vim')
 vim.call('plug#end')
---
---
-vim.cmd "set title"
---print("hi")
---
+
 -- globals
+--
 vim.env.FZF_DEFAULT_OPTS = '--layout=reverse'
 vim.opt.tabstop = 2
 vim.opt.wildignore = {'*/cache/*', '*/tmp/*'}
---colorscheme "darkblue"
+-- vim.opt.colorscheme "darkblue"
---
+++ profile-nix/.profile
@@ -0,0 +1,38 @@
+#!/bin/sh
+# make default editor Neovim
+export EDITOR=nvim
+export FZF_DEFAULT_COMMAND='rg --files --hidden --glob \!.git --glob \!.cache --glob \!.local --glob \!.nvm --glob \!.npm --glob \!.mozilla'
+
+echo "loaded .profile"
+
+# Most pure GTK3 apps use wayland by default, but some,
+# like Firefox, need the backend to be explicitely selected.
+export MOZ_ENABLE_WAYLAND=1
+export MOZ_DBUS_REMOTE=1
+export GTK_CSD=0
+
+# qt wayland
+export QT_QPA_PLATFORM="wayland"
+export QT_QPA_PLATFORMTHEME=qt5ct
+export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+
+#Java XWayland blank screens fix
+export _JAVA_AWT_WM_NONREPARENTING=1
+
+# set default shell and terminal
+export SHELL=/usr/bin/zsh
+export TERMINAL_COMMAND=/usr/share/sway/scripts/foot.sh
+
+alias g=git
+alias r=ranger
+alias f=fzf
+alias v=nvim
+
+# prompt
+# source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
+
+# fzf
+#source $HOME/Config/scripts/key-bindings.zsh
+#source $HOME/Config/scripts/completion.zsh
+#source $HOME/Config/scripts/tab-completion.zsh
+
