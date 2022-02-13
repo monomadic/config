3260c3a3fe8b059306e087d1a77e42e61c775c1e
nom <nom@nowhere>
2022-02-13 15:32:18 +0800

    nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -542,6 +542,51 @@ catppuccin.setup({
   },
 })
 
+require('material').setup({
+
+	contrast = {
+		sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+		floating_windows = false, -- Enable contrast for floating windows
+		line_numbers = false, -- Enable contrast background for line numbers
+		sign_column = false, -- Enable contrast background for the sign column
+		cursor_line = false, -- Enable darker background for the cursor line
+		non_current_windows = false, -- Enable darker background for non-current windows
+		popup_menu = true, -- Enable lighter background for the popup menu
+	},
+
+	italics = {
+		comments = false, -- Enable italic comments
+		keywords = false, -- Enable italic keywords
+		functions = false, -- Enable italic functions
+		strings = false, -- Enable italic strings
+		variables = false -- Enable italic variables
+	},
+
+	contrast_filetypes = { -- Specify which filetypes get the contrasted (darker) background
+		"terminal", -- Darker terminal background
+		"packer", -- Darker packer background
+		"qf" -- Darker qf list background
+	},
+
+	high_visibility = {
+		lighter = false, -- Enable higher contrast text for lighter style
+		darker = false -- Enable higher contrast text for darker style
+	},
+
+	disable = {
+		borders = false, -- Disable borders between verticaly split windows
+		background = false, -- Prevent the theme from setting the background (NeoVim then uses your teminal background)
+		term_colors = false, -- Prevent the theme from setting terminal colors
+		eob_lines = false -- Hide the end-of-buffer lines
+	},
+
+	lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
+
+	async_loading = true, -- Load parts of the theme asyncronously for faster startup (turned on by default)
+
+	custom_highlights = {} -- Overwrite highlights with your own
+})
+
 -- cmd([[hi NvimTreeVertSplit guibg=none]])
 
 vim.opt.termguicolors = true
@@ -550,8 +595,6 @@ vim.opt.termguicolors = true
 --   hi Normal guibg=none ctermbg=none
 --   hi Pmenu ctermfg=white guibg=#222222 ctermbg=234 ctermfg=246
 --   hi Folded ctermbg=DarkGrey ctermfg=White guibg=#222222 guifg=#FFFFFF
---   hi NERDTREEDir gui=none ctermfg=120 cterm=none
---   hi NERDTreeCWD cterm=none gui=none ctermfg=62
 --   hi VertSplit ctermfg=none gui=none guibg=none
 --   hi Comment guifg=#666666 ctermfg=grey
 --   hi Cursor ctermbg=0 ctermfg=none guibg=0 guibg=none guifg=#FFFFFF
@@ -561,7 +604,7 @@ vim.opt.termguicolors = true
 -- inline colors
 --require'colorizer'.setup()
 
-cmd([[colorscheme tokyonight]])
+cmd([[colorscheme material]])
 -- }}}
 
 -- require("bufferline").setup({
