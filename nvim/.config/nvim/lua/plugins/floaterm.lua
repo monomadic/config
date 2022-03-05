04e8dc5e451dad5539e751ed9137d01c41b098eb
nom <nom@nowhere>
2022-03-05 08:47:19 +0800

    kitty

--- nvim/.config/nvim/lua/plugins/floaterm.lua
+++ nvim/.config/nvim/lua/plugins/floaterm.lua
@@ -4,19 +4,19 @@ vim.g.floaterm_height = 0.8
 vim.g.floaterm_keymap_new = "<F7>"
 vim.g.floaterm_keymap_prev = "<F8>"
 vim.g.floaterm_keymap_next = "<F9>"
-vim.g.floaterm_keymap_toggle = "<C-m>"
--- vim.g.floaterm_autoclose = 2
+vim.g.floaterm_keymap_toggle = "<C-n>"
+vim.g.floaterm_autoclose = 0
 vim.g.floaterm_opener = "edit" -- edit | split | vsplit | tabe | drop
 
 local map = vim.api.nvim_set_keymap
-map("n", "<C-m>", ":FloatermToggle<CR>", { silent = true })
+map("n", "<C-n>", ":FloatermToggle<CR>", { silent = true })
 map("n", "<C-g>", ":FloatermNew --title=ranger ranger<CR>", { silent = true })
---map("n", "<C-m>", ":FloatermNew --title=broot broot<CR>", { silent = true })
+map("n", "<C-m>", ":FloatermNew --title=broot broot<CR>", { silent = true })
 
 vim.cmd([[command! -nargs=1 Rg :FloatermNew rg <args>]])
 vim.cmd([[command! Broot :FloatermNew broot]])
 
 vim.cmd([[
   hi Floaterm guibg=black
-  hi FloatermBorder guifg=cyanA
+  hi FloatermBorder guifg=cyan
 ]])
