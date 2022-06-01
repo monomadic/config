355a5fd51c27407c45d69ad17226989be527210d
monomadic <monomadic@monomadic>
2022-06-01 23:14:27 +0700

    better dotter org

--- sway-simple/.config/sway/scripts/waybar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# wrapper script for waybar with args, see https://github.com/swaywm/sway/issues/5724
-
-USER_CONFIG_PATH=$HOME/.config/sway/apps/waybar/config.jsonc
-USER_STYLE_PATH=$HOME/.config/sway/apps/waybar/style.css
-
-waybar -c ${USER_CONFIG_PATH} -s ${USER_STYLE_PATH} &
