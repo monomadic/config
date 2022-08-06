a7cb091e187b27b3aa72ecc52c27258d24c79594
monomadic <monomadic.crypto>
2022-08-06 21:38:49 +0700

    custom term popup

--- neovim.lua
+++ neovim.lua
@@ -87,6 +87,75 @@ end })
 -- 	vim.cmd("FloatermToggle")
 -- end)
 
+vim.keymap.set('t', '<C-Space>', function()
+	local win = vim.api.nvim_get_current_win()
+	vim.api.nvim_win_close(win, true)
+end)
+
+vim.keymap.set('n', '<C-Space>', function()
+	local buf = vim.api.nvim_create_buf(false, true) -- new buffer for the term
+	-- local selected_file = vim.fn.expand('%:p') -- the currently open filename
+
+	vim.api.nvim_buf_set_option(buf, "filetype", "terminal")
+	vim.api.nvim_buf_set_option(buf, "buflisted", false) -- don't show in bufferlist
+	vim.api.nvim_open_win(buf, true, { -- true here focuses the buffer
+		relative = 'editor',
+		row = math.floor(0.05 * vim.o.lines),
+		col = math.floor(0.1 * vim.o.columns),
+		width = math.ceil(0.8 * vim.o.columns),
+		height = math.ceil(0.7 * vim.o.lines),
+		border = 'single'
+	})
+
+	local win = vim.api.nvim_get_current_win()
+	vim.api.nvim_win_set_buf(win, buf)
+	vim.wo.relativenumber = false -- turn off line numbers
+	vim.wo.number = false
+
+	vim.fn.termopen(vim.o.shell)
+	-- vim.api.nvim_chan_send(job_id, "lf\n")
+
+	vim.cmd "startinsert" -- start in insert mode
+
+	-- local lf_tmpfile = vim.fn.tempname()
+	-- local lf_tmpdir = vim.fn.tempname()
+	--
+	-- local process_cmd = 'lf -last-dir-path="' ..
+	-- 		lf_tmpdir .. '" -selection-path="' .. lf_tmpfile .. '" '
+	--
+	-- if selected_file ~= "" then
+	-- 	process_cmd = process_cmd .. '"' .. selected_file .. '"'
+	-- end
+	--print(process_cmd)
+
+	-- launch lf process
+	-- vim.fn.termopen(process_cmd, {
+	-- 	-- on_exit = function() -- job_id, exit_code, event_type
+	-- 	--
+	-- 	-- 	-- -- if window is a float, close the window
+	-- 	-- 	-- if vim.api.nvim_win_get_config(win).zindex then
+	-- 	-- 	-- 	vim.api.nvim_win_close(win, true)
+	-- 	-- 	-- end
+	-- 	-- 	-- if lf correctly left us a tempfile
+	-- 	-- 	if vim.loop.fs_stat(lf_tmpfile) then
+	-- 	-- 		local contents = {}
+	-- 	-- 		-- grab the entries that were selected (one per line)
+	-- 	-- 		for line in io.lines(lf_tmpfile) do
+	-- 	-- 			table.insert(contents, line)
+	-- 	-- 		end
+	-- 	-- 		if not vim.tbl_isempty(contents) then
+	-- 	-- 			--vim.api.nvim_win_close(0, true) -- close current (0) with force
+	-- 	--
+	-- 	-- 			for _, fname in pairs(contents) do
+	-- 	-- 				-- and open them for editing
+	-- 	-- 				vim.cmd(("%s %s"):format('edit', fname))
+	-- 	-- 			end
+	-- 	-- 		end
+	-- 	-- 	end
+	-- 	-- end,
+	-- })
+end)
+
 -- custom terminal float
 vim.keymap.set('n', '<C-p>', function()
 	local buf = vim.api.nvim_create_buf(false, true) -- new buffer for the term
@@ -574,7 +643,7 @@ require('packer').startup(function(use)
 	}
 
 	use { 'vimwiki/vimwiki', config = function()
-		vim.keymap.set("n", "gi", "<Cmd>VimwikiIndex<CR>")
+		vim.keymap.set("n", "gi", "<Cmd>VimwikiIndex<CR>") -- TODO: lsp variants (eg rust will look for lib.rs, main.rs etc)
 		vim.keymap.set("n", "gw", "<Cmd>VimwikiGoto ")
 		vim.g.vimwiki_list = {
 			{
