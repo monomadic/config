72a06b3bc8b70ae40a985cdb96f0360f03cd835f
monomadic <monomadic.crypto>
2022-12-14 15:24:38 +0800

    removed zgen dependency

---
+++ zsh/lf-jump.zsh
@@ -0,0 +1,16 @@
+# lf directory change
+function lfcd() {
+	local tmp="$(mktemp)"
+	lf -last-dir-path="$tmp" "$@"
+	if [ -f "$tmp" ]; then
+			dir="$(cat "$tmp")"
+			rm -f "$tmp"
+			if [ -d "$dir" ]; then
+					if [ "$dir" != "$(pwd)" ]; then
+							cd "$dir"
+							# clear
+							zle && zle reset-prompt
+					fi
+			fi
+	fi
+}
