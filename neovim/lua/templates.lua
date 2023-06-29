21b7ce8a2a609af75028e38c077b352480adab43
monomadic <monomadic@localhost>
2023-06-29 17:36:54 +0800

    update:

--- neovim/lua/templates.lua
+++ neovim/lua/templates.lua
@@ -4,28 +4,73 @@ M.new_file_from_template = function()
 	local pickers = require("telescope.pickers")
 	local sorters = require("telescope.sorters")
 	local finders = require("telescope.finders")
+	local Path = require("plenary.path")
+	local entry_display = require("telescope.pickers.entry_display")
+	local devicons = require("nvim-web-devicons")
 
 	local filetype = vim.bo.filetype
 	if filetype and filetype ~= '' then
 		filetype = filetype .. "/"
 	end
 
+	local function get_files_recursive(directory)
+		local files = {}
+		local p = io.popen('fd --type symlink . "' .. directory .. '"')
+		if p then
+			for entry in p:lines() do
+				local relative_path = entry
+				table.insert(files, relative_path)
+			end
+		end
+		return files
+	end
+
 	local template_dir = vim.fn.expand("~/.config/nvim/templates/" .. filetype)
 	local current_directory = vim.fn.expand('%:p:h')
 
 	pickers.new {
-		results_title = "Insert Template",
-		finder = finders.new_oneshot_job({ "fd", "--type", "symlink", ".", template_dir }),
+		prompt_title = "Insert Template",
+		finder = finders.new_table {
+			results = get_files_recursive(template_dir),
+			entry_maker = function(entry)
+				local file_path = Path:new(entry)
+				local icon, icon_highlight = devicons.get_icon(file_path.filename, file_path.filetype)
+				local displayer = entry_display.create({
+					separator = " ",
+					items = {
+						{ width = 1 },
+						{ remaining = true },
+					}
+				})
+				return {
+					display = function(entry)
+						return displayer({
+							{ icon,          icon_highlight },
+							{ entry.filename }
+						})
+					end,
+					ordinal = file_path.filename,
+					value = entry,
+					filename = file_path.filename,
+					display_cols = {
+						{ icon,              icon_highlight },
+						{ file_path.filename },
+					}
+				}
+			end
+		},
+
 		sorter = sorters.get_generic_fuzzy_sorter(),
+
 		previewer = require 'telescope.previewers'.vim_buffer_cat.new({}),
+
 		attach_mappings = function(bufnr, map)
 			local actions = require 'telescope.actions'
 			map("i", "<CR>", function()
-				local selected_file = require('telescope.actions.state').get_selected_entry()[1]
+				local selected_file = require('telescope.actions.state').get_selected_entry().value
 				if not selected_file then
 					return
 				end
-
 				vim.ui.input({ prompt = 'Save template as: ', default = current_directory .. '/', completion = 'dir' },
 					function(destination_file)
 						if not destination_file then
