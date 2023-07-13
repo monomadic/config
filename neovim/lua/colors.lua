f4b275dfda9bb54d40fad9ae2121b00bca0433b1
monomadic <monomadic@localhost>
2023-07-13 18:55:32 +0800

    Change document_grey color to "#1E1D3D" and update TITLEBAR_BG variable to "#1E1D3D".

--- neovim/lua/colors.lua
+++ neovim/lua/colors.lua
@@ -1,7 +1,6 @@
 -- COLORS
 -- https://ofstack.com/Linux/25280/vim-custom-highlighted-groups-and-some-practical-tips.html
 
-local TITLEBAR_BG = "#1E1D3D"
 -- local TAB_BG = "#323246"
 
 local M = {}
@@ -28,16 +27,14 @@ M.telescope = function()
 	hl(0, "CursorLine", { bg = results_bg })
 end
 
--- color picker
--- nvim-colortils/colortils.nvim
-
 autocmd({ "ColorScheme", "VimEnter" },
 	{
 		pattern = "*",
 		callback = function()
-			local document_grey = "#1E1D2D";
+			local document_grey = "#1E1D3D";
 			local dark_grey = "#111122";
 			local light_grey = "#323246";
+			local TITLEBAR_BG = "#1E1D3D"
 
 			-- local title_bar = { bg = dark_grey };
 			-- local tabs = { fg = "white", bg = dark_grey };
@@ -47,11 +44,17 @@ autocmd({ "ColorScheme", "VimEnter" },
 			local tabs = { fg = "white", bg = light_grey };
 			local tabs_bg = { bg = "None" };
 
+			hl(0, "ScrollbarHandle", { bg = document_grey })
+
 			-- hl(0, "BarDiagnosticError", { fg = "white", bg = "#F02282" })
 			-- hl(0, "BarDiagnosticHint", { fg = "white", bg = "#F0F0AA" })
 			-- hl(0, "BarDiagnosticInformation", { fg = "white", bg = "#3070FF" })
 			-- hl(0, "BarDiagnosticWarn", { fg = "white", bg = "#FFFF00" })
 			-- hl(0, "LineNr", { fg = "#222222" }) -- active
+			-- hl(0, "TabLineFill", { fg = "white", bg = "#262639" })
+			-- hl(0, "WinBarNC", { fg = "white", bg = "#2222FF" })
+			-- hl(0, "WinSeparator", { fg = "bg", bg = "bg" }) -- inactive
+
 			hl(0, "CursorLine", { bg = "black" })
 			hl(0, "DiagnosticUnderlineError", { fg = "#F02282", underdotted = true })
 			hl(0, "DiagnosticUnderlineWarn", { fg = "#F0F0AA", underdotted = true })
@@ -62,24 +65,27 @@ autocmd({ "ColorScheme", "VimEnter" },
 			hl(0, "FzfLuaNormal", { fg = "white", bg = "black" })
 			hl(0, "LspActive", { fg = "#00FF00" })
 			hl(0, "NormalFloat", { bg = "black" }) -- terminal?
-			hl(0, "StatusLine", {})            -- active
-			hl(0, "StatusLineNC", {})          -- inactive
+			hl(0, "StatusLine", {})             -- active
+			hl(0, "StatusLineNC", {})           -- inactive
 			hl(0, "TabLine", { fg = "white", bg = "black" })
 			hl(0, "TabLineFill", { bg = TITLEBAR_BG })
-			--hl(0, "TabLineFill", { fg = "white", bg = "#262639" })
-			hl(0, "Title", { fg = "#CCFF00" })
 			hl(0, "TodoBgTODO", { bg = "#FFFF00", fg = "black" })
 			hl(0, "TodoFgTODO", { fg = "#FFFF00" })
 			hl(0, "WinBar", tabs_bg)
 			hl(0, "WinBarFileName", tabs)
-			-- hl(0, "WinBarNC", { fg = "white", bg = "#2222FF" })
-			--hl(0, "WinSeparator", { fg = "bg", bg = "bg" }) -- inactive
 
-			hl(0, "VimwikiBold", { fg = "#AFAAFF" })
+			-- #CCFF00
+			-- #FFFF00
+			-- #CC88FF
+			-- #33CCFF
+			-- #CC00FF
+
+			hl(0, "Title", { fg = "#F1FA8C" })
+			hl(0, "VimwikiBold", { fg = "#CC88FF" })
 			hl(0, "VimwikiItalic", { fg = "#FFFF00" })
 			hl(0, "VimwikiLink", { fg = "#33CCFF" })
 			--hl(0, "VimwikiLink", { fg = "#3377FF", underdashed = true })
-			hl(0, "VimwikiHeaderChar", { fg = "#F1FA8C" })
+			hl(0, "VimwikiHeaderChar", { link = "Comment" })
 			hl(0, "VimwikiCode", { bg = "#222222", fg = "#F1FA8C" })
 
 			hl(0, "MsgArea", { bg = "None" }) -- CommandLine
@@ -87,12 +93,5 @@ autocmd({ "ColorScheme", "VimEnter" },
 			M.telescope()
 		end
 	})
-
--- https://gist.github.com/romainl/379904f91fa40533175dfaec4c833f2f
---vim.cmd("colorscheme {{colorscheme}}");
-
--- vim.api.nvim_create_autocmd("FileType", { pattern = "rust", callback = function()
--- 	vim.cmd("colorscheme habamax");
--- end })
 
 return M
