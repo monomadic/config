46ced9cae9c4d5fe14486e354aa7c10221d300ed
monomadic <monomadic@localhost>
2023-07-13 16:17:28 +0800

    refactor: improve GitPush function and add start insert mode

--- neovim/lua/git.lua
+++ neovim/lua/git.lua
@@ -1,25 +1,26 @@
+local function open_floating_terminal_with_command(command)
+	local buf = vim.api.nvim_create_buf(false, true)
+	local width = vim.api.nvim_get_option("columns")
+	local height = vim.api.nvim_get_option("lines")
+
+	vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
+
+	local win = vim.api.nvim_open_win(buf, true, {
+		relative = "editor",
+		width = math.ceil(width * 0.8),
+		height = math.ceil(height * 0.8),
+		col = math.ceil(width * 0.1),
+		row = math.ceil(height * 0.1),
+		style = "minimal",
+		border = "single"
+	})
+
+	vim.api.nvim_win_set_option(win, "winhl", "Normal:NormalFloat")
+	vim.cmd.startinsert() -- start in insert mode
+	vim.cmd("terminal " .. command)
+end
+
 function GitPush()
-	local function open_floating_terminal_with_command(command)
-		local buf = vim.api.nvim_create_buf(false, true)
-		local width = vim.api.nvim_get_option("columns")
-		local height = vim.api.nvim_get_option("lines")
-
-		vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
-
-		local win = vim.api.nvim_open_win(buf, true, {
-			relative = "editor",
-			width = math.ceil(width * 0.8),
-			height = math.ceil(height * 0.8),
-			col = math.ceil(width * 0.1),
-			row = math.ceil(height * 0.1),
-			style = "minimal",
-			border = "single"
-		})
-
-		vim.api.nvim_win_set_option(win, "winhl", "Normal:NormalFloat")
-		vim.cmd("terminal " .. command)
-	end
-
 	open_floating_terminal_with_command("git push")
 end
 
@@ -38,30 +39,17 @@ function AiCommit()
 	end
 
 	-- add all changed files to staging
-	vim.fn.system('!git add -A')
+	vim.fn.system('git add -A')
 
 	-- check if there are changes to commit
-	handle = io.popen('git diff --cached --exit-code')
-	if handle == nil then
-		return
-	end
-	result = handle:read("*a")
-	handle:close()
+	local exit_code = os.execute('git diff --cached --exit-code')
 
-	if result:find("changed") == nil then
+	if exit_code == 0 then
 		print("There are no changes to commit.")
 		return
 	end
 
-	-- prompts for a commit message
-	vim.ui.input({ prompt = "Commit message: ", default = "add: " }, function(message)
-		if not message then
-			return
-		end
-		-- creates the commit
-		vim.cmd('!aicommit')
-		print("Commit successful")
-	end)
+	open_floating_terminal_with_command("aicommits")
 end
 
 function QuickCommit()
