be488a37cd077bcd21e40c38de21c117a1d7077a
monomadic <monomadic@localhost>
2023-07-13 16:13:01 +0800

    add: function to automate git commit

--- neovim/lua/git.lua
+++ neovim/lua/git.lua
@@ -23,6 +23,47 @@ function GitPush()
 	open_floating_terminal_with_command("git push")
 end
 
+function AiCommit()
+	local handle = io.popen('git rev-parse --is-inside-work-tree')
+	if handle == nil then
+		return
+	end
+
+	local result = handle:read("*a")
+	handle:close()
+
+	if result:find('true') == nil then
+		print("This directory is not a git repository.")
+		return
+	end
+
+	-- add all changed files to staging
+	vim.fn.system('!git add -A')
+
+	-- check if there are changes to commit
+	handle = io.popen('git diff --cached --exit-code')
+	if handle == nil then
+		return
+	end
+	result = handle:read("*a")
+	handle:close()
+
+	if result:find("changed") == nil then
+		print("There are no changes to commit.")
+		return
+	end
+
+	-- prompts for a commit message
+	vim.ui.input({ prompt = "Commit message: ", default = "add: " }, function(message)
+		if not message then
+			return
+		end
+		-- creates the commit
+		vim.cmd('!aicommit')
+		print("Commit successful")
+	end)
+end
+
 function QuickCommit()
 	local handle = io.popen('git rev-parse --is-inside-work-tree')
 	if handle == nil then
