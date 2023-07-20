b7bb3e766b5232b89f05ea99abedc821c9efb6e2
monomadic <monomadic@localhost>
2023-07-20 13:21:38 +0800

    update:

--- neovim/lua/plugins/diffview.lua
+++ neovim/lua/plugins/diffview.lua
@@ -1,6 +1,11 @@
+-- git merge conflict resolution
 return {
 	'sindrets/diffview.nvim',
 	config = function()
+		local key = require('utils').key
+
+		key('n', '<leader>gr', ':DiffviewOpen<CR>', 'resolve conflicts')
+
 		require("diffview").setup({})
 	end
 }
