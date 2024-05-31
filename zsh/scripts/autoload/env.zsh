870d0e4143c5a4ea9880408091c3baa655d927f8
monomadic <129359+monomadic@users.noreply.github.com>
2024-05-31 09:24:32 +0800

    update:

--- zsh/scripts/autoload/env.zsh
+++ zsh/scripts/autoload/env.zsh
@@ -18,3 +18,6 @@ export PATH=$PATH:$WASMTIME_HOME/bin
 # Set default language and character encoding
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
+
+export babyblue=/Volumes/BabyBlue2TB
+export babyblueinbox=/Volumes/BabyBlue2TB/not-porn/___full-videos/_inbox
