d9bedf3bd6977ea77fe72cbdc81683b771ca381f
monomadic <129359+monomadic@users.noreply.github.com>
2024-06-29 17:07:21 +0800

    update:

--- zsh/scripts/autoload/completion.zsh
+++ zsh/scripts/autoload/completion.zsh
@@ -3,8 +3,9 @@
 autoload -Uz compinit
 compinit
 
-# use fzf for shell completion
-source $ZSH_CONFIG_DIR/autoload/fzf-zsh-completion.sh
+# use fzf for shell completion (this should autoload?)
+source $ZSH_CONFIG_DIR/completions/fzf-zsh.sh
+source $ZSH_CONFIG_DIR/completions/things.zsh
 
 # 1password
 eval "$(op completion zsh)"
