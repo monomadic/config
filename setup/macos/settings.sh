8d4d302e3c3a8f469ebedbaa68d1eb1289a8fa0f
monomadic <129359+monomadic@users.noreply.github.com>
2024-10-30 15:02:01 +0800

    update:

---
+++ setup/macos/settings.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# ~/Library/Preferences/com.apple.ScreenContinuity.plist
+#
+# resize the iphone mirroring window
+defaults write com.apple.ScreenContinuity showScalingControls -bool true
