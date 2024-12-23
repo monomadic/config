e41bf2ad4f16736ba8007fe42b9ec3d6782e2d1b
monomadic <129359+monomadic@users.noreply.github.com>
2024-12-23 23:37:45 +0800

    update:

--- zsh/scripts/autoload/fzf.zsh
+++ zsh/scripts/autoload/fzf.zsh
@@ -1,9 +1,11 @@
 # themes
 # export FZF_THEME_MOLOKAI='--color=bg+:#293739,bg:#1B1D1E,border:#808080,spinner:#E6DB74,hl:#7E8E91,fg:#F8F8F2,header:#7E8E91,info:#A6E22E,pointer:#A6E22E,marker:#F92672,fg+:#F8F8F2,prompt:#F92672,hl+:#F92672'
-# export FZF_DEFAULT_OPTS=$FZF_THEME_MOLOKAI
 
 # history
 # bind '"\C-r": "$(fc -rl 1 | fzf -e)"'
+
+# source included shell integrations
+source <(fzf --zsh)
 
 # ssh into known_hosts
 function fzf-ssh() {
