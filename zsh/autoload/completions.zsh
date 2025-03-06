85da6bec4ae240abfaa234cdad1a7fbd2db1c256
monomadic <129359+monomadic@users.noreply.github.com>
2025-03-06 18:41:47 +0800

    Auto-sync from Mac.lan on 2025-03-06 18:41:47

--- zsh/autoload/completions.zsh
+++ zsh/autoload/completions.zsh
@@ -11,7 +11,11 @@ setopt autopushd
 autoload -Uz add-zsh-hook
 
 # Completion styles
+#
+#  navigate completion suggestions using arrow keys
 zstyle ':completion:*' menu select
+#
+#  zstyle ':completion:*' format '%B%d%b'
 zstyle ':completion:*' special-dirs true
 zstyle ':completion:*' list-dirs first
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
