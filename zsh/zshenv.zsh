5919a79457f653ce6254fc3c8f0ef4ba913876b9
monomadic <129359+monomadic@users.noreply.github.com>
2025-12-31 06:28:46 +0800

    update:

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -98,7 +98,7 @@ export LC_ALL="en_US.UTF-8"
 export GHQ_ROOT=$HOME/src
 
 # FZF / Skim
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --bind 'alt-r:execute-silent(open --reveal {})' --bind 'alt-s:select-all+accept' --layout=reverse --cycle --preview-window=noborder --highlight-line --no-separator --no-border --inline-info --bind 'ctrl-u:unix-line-discard' --color=bg:-1,fg:blue,info:15,header:7,hl:red,hl+:red,gutter:-1,prompt:yellow,marker:-1,bg+:black,pointer:yellow,fg+:yellow"
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --bind 'alt-r:execute-silent(open --reveal {})' --bind 'alt-s:select-all+accept' --layout=reverse --cycle --preview-window=noborder --highlight-line --no-separator --gutter=' ' --no-border --inline-info --bind 'ctrl-u:unix-line-discard' --color=bg:-1,fg:blue,info:15,header:7,hl:red,hl+:red,gutter:-1,prompt:yellow,marker:-1,bg+:black,pointer:yellow,fg+:yellow"
 export FZF_COMPLETION_TRIGGER='\t' # Default is '**'
 export FZF_COMPLETION_OPTS='--preview "bat --color=always {} 2>/dev/null || cat {} 2>/dev/null"'
 export FZF_PREVIEW_COMMAND='fzf-preview {}'
